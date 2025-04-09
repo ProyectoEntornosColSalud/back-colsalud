6943faad1aad515530826020acbcef6a7210af23
jhonc <calderonjhondairo@gmail.com>
2025-04-09 16:28:53 -0500

    add vite

--- src/main/java/com/calderon/denv/pep/security/SecurityConfig.java
+++ src/main/java/com/calderon/denv/pep/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.calderon.denv.pep.security;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,9 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -72,8 +70,7 @@ public class SecurityConfig {
   private CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5173"));
-    config.addAllowedOriginPattern("*");
+    config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("Content-Type", "Authorization"));
     config.setExposedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
